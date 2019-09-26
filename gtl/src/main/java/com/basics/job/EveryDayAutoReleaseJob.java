c63c3c164ee45f8d312ccda21063c0f19642a9ab
quanlun_liu <liuquanlun2007@163.com>
2019-09-26 17:22:21 +0800

    1

--- gtl/src/main/java/com/basics/job/EveryDayAutoReleaseJob.java
+++ gtl/src/main/java/com/basics/job/EveryDayAutoReleaseJob.java
@@ -8,6 +8,9 @@ import com.basics.support.CommonSupport;
 import com.basics.support.LogUtils;
 import com.basics.support.QueryFilterBuilder;
 import com.basics.support.job.EveryDayJob;
+import com.basics.wallet.controller.dao.WalletBaseDao;
+import com.basics.wallet.controller.entity.WalletEntity;
+import com.qiniu.util.StringUtils;
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,13 +29,23 @@ public class EveryDayAutoReleaseJob extends BaseApiService implements EveryDayJo
 	@Override
 	public void doJob() {
 		LogUtils.performance.info("{} begin at {}", this.getClass().getName(), new Date());
-		List<OrOrder> orders = orOrderDao.query(new QueryFilterBuilder().put("orderStatus", Constant.ORDER_STATUS_2).put("orderConfirm",new Date()).build());
-		if (CollectionUtils.isNotEmpty(orders)){
-			for (OrOrder order : orders){
-				CuCustomerAccount account = cuCustomerAccountDao.get(order.getCustomerId());
-				CommonSupport.checkNotNull(account, "用户account的错误");
-				if(account.getLockMoney().doubleValue() >= order.getOrderPayPrice().doubleValue()) {
-					createActiveMq(account.getId(), Constant.ACTIVEMQ_TYPE_34, BigDecimal.ZERO, order.getId(), "商城--自动取消",null);
+		List<WalletEntity> records = walletReleaseDao.query(new QueryFilterBuilder().put("orderStatus", Constant.ORDER_STATUS_2).build());
+		if (CollectionUtils.isNotEmpty(records)){
+			for (WalletEntity record : records){
+
+//				CuCustomerAccount account = cuCustomerAccountDao.get(order.getCustomerId());
+//				CommonSupport.checkNotNull(account, "用户account的错误");
+				if(!StringUtils.isNullOrEmpty(record.getSuperNum())) {
+					createActiveMq(record.getUserId(), Constant.ACTIVEMQ_TYPE_42, BigDecimal.ZERO, record.getId(), "流通钱包释放",null);
+				}
+				if(!StringUtils.isNullOrEmpty(record.getmTokenNum())) {
+					createActiveMq(record.getUserId(), Constant.ACTIVEMQ_TYPE_43, BigDecimal.ZERO, record.getId(), "MTOKEN释放",null);
+				}
+				if(!StringUtils.isNullOrEmpty(record.getSuperNum())) {
+					createActiveMq(record.getUserId(), Constant.ACTIVEMQ_TYPE_42, BigDecimal.ZERO, record.getId(), "超级钱包释放",null);
+				}
+				if(!StringUtils.isNullOrEmpty(record.getMoveNum())) {
+					createActiveMq(record.getUserId(), Constant.ACTIVEMQ_TYPE_42, BigDecimal.ZERO, record.getId(), "流通钱包释放",null);
 				}
 			}
 		}
--- gtl/src/main/java/com/basics/wallet/controller/dao/WalletReleaseDao.java
+++ gtl/src/main/java/com/basics/wallet/controller/dao/WalletReleaseDao.java
@@ -1,7 +1,8 @@
 package com.basics.wallet.controller.dao;
 import com.basics.or.entity.OrOrder;
 import com.basics.support.GenericDao;
+import com.basics.wallet.controller.entity.WalletEntity;
 
-public interface WalletReleaseDao extends GenericDao<WalletEntityDao> {
+public interface WalletReleaseDao extends GenericDao<WalletEntity> {
 
 }
